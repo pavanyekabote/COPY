0f1e5e5ecc97b9cdd3c371d2a1d570ee807ce4cf
Pavan Yekabote <yekabotep@gmail.com>
2017-08-04 23:22:48 -0400

    Changes to YouDownload.java

--- src/YouDownload.java
+++ src/YouDownload.java
@@ -16,7 +16,7 @@ public class YouDownload {
 		ucon.setRequestMethod("GET");
 		ucon.connect();
 		InputStream in = ucon.getInputStream();
-		fout = new FileOutputStream(new File("/home/pavan/PYShare/data.swf"));
+		fout = new FileOutputStream(new File("/root/Desktop/data.swf"));
 		while((c = in.read(buf,0,Store.MAX_DATA_LENGTH))!= -1)
 		{
 			fout.write(buf, 0, c);
@@ -28,6 +28,7 @@ public class YouDownload {
 
 	public static void main(String args[])throws Exception
 	{
-		new YouDownload("https://www.youtube.com/v/xLjsZr9KDuw?version=3&amp;autohide=1");
+		//new YouDownload("https://www.youtube.com/v/xLjsZr9KDuw?version=3&amp;autohide=1");
+		new YouDownload("https://youtube.com/embed/wTphObvEH9w");
 	}
 }
